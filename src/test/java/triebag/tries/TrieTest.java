b9a8da773134c6ab280f4733b7738f5edb254c79
Oliver Schrenk <oliver.schrenk@gmail.com>
2010-10-26 15:15:51 +0200

    Removed Foo class for testing, replaced with String

--- src/test/java/triebag/tries/TrieTest.java
+++ src/test/java/triebag/tries/TrieTest.java
@@ -12,73 +12,61 @@ public class TrieTest {
 
 	@Test(expected = NullPointerException.class)
 	public void testNullCharStream() throws NullPointerException {
-		Trie<Foo> ptrie = new Trie<Foo>();
-		ptrie.add(null, new Foo());
+		Trie<String> ptrie = new Trie<String>();
+		ptrie.add(null, new String());
 	}
 
 	@Test
 	public void testEmptyTrie() {
-		Trie<Foo> ptrie = new Trie<Foo>();
+		Trie<String> ptrie = new Trie<String>();
 		assertEquals(0, toList(ptrie.getItemsWithPrefix("foo")).size());
 	}
 
 	@Test
 	public void testSingleInsertion() {
-		Trie<Foo> ptrie = new Trie<Foo>();
-		Foo b = new Foo();
-		ptrie.add("foo", b);
-		assertEquals(b, ptrie.getItem("foo"));
+		Trie<String> ptrie = new Trie<String>();
+		ptrie.add("foo", "a");
+		assertEquals("a", ptrie.getItem("foo"));
 	}
 
 	@Test
 	public void testSameStringInsertionsReplaces() {
-		Trie<Foo> ptrie = new Trie<Foo>();
-		Foo b = new Foo();
-		Foo b2 = new Foo();
-		ptrie.add("foo", b);
-		ptrie.add("foo", b2);
-		assertEquals(b2, ptrie.getItem("foo"));
+		Trie<String> ptrie = new Trie<String>();
+		ptrie.add("foo", "a");
+		ptrie.add("foo", "b");
+		assertEquals("b", ptrie.getItem("foo"));
 	}
 
 	@Test
-	public void testDifferentStrInsertion() {
-		Trie<Foo> ptrie = new Trie<Foo>();
-		Foo b = new Foo();
-		Foo b2 = new Foo();
-		ptrie.add("foo", b);
-		ptrie.add("bar", b2);
-		assertEquals(b, ptrie.getItem("foo"));
-		assertEquals(b2, ptrie.getItem("bar"));
+	public void testDifferentStringInsertion() {
+		Trie<String> ptrie = new Trie<String>();
+		ptrie.add("foo", "a");
+		ptrie.add("bar", "b");
+		assertEquals("a", ptrie.getItem("foo"));
+		assertEquals("b", ptrie.getItem("bar"));
 	}
 
 	@Test
-	public void testSubStrInsertion() {
-		Trie<Foo> ptrie = new Trie<Foo>();
-		Foo b = new Foo();
-		Foo b2 = new Foo();
-		ptrie.add("foo", b);
-		ptrie.add("foobar", b2);
-		assertEquals(b, ptrie.getItem("foo"));
-		assertEquals(b2, ptrie.getItem("foobar"));
-		ptrie = new Trie<Foo>();
-		ptrie.add("foobar", b);
-		ptrie.add("foo", b2);
-		assertEquals(b, ptrie.getItem("foobar"));
-		assertEquals(b2, ptrie.getItem("foo"));
+	public void testSubStringInsertion() {
+		Trie<String> ptrie = new Trie<String>();
+		ptrie.add("foo", "a");
+		ptrie.add("foobar", "b");
+		assertEquals("a", ptrie.getItem("foo"));
+		assertEquals("b", ptrie.getItem("foobar"));
+
+		ptrie = new Trie<String>();
+		ptrie.add("foobar", "a");
+		ptrie.add("foo", "b");
+		assertEquals("a", ptrie.getItem("foobar"));
+		assertEquals("b", ptrie.getItem("foo"));
 	}
 
-	private List<Foo> toList(final Iterator<Foo> it) {
-		ArrayList<Foo> list = new ArrayList<Foo>();
+	private List<String> toList(final Iterator<String> it) {
+		ArrayList<String> list = new ArrayList<String>();
 		while (it.hasNext()) {
 			list.add(it.next());
 		}
 		return list;
-	}
-
-	/**
-	 * Empty inner class for test purposes
-	 */
-	class Foo {
 	}
 
 }
